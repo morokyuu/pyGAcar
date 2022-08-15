sign fixed.

car_model.py
@@ -13,7 +13,7 @@ WINDOW_H = 480
 def rot(th):
     return np.array([
         [np.cos(th), -np.sin(th), 0.0],
-        [np.sin(th), np.cos(th), 0.0],
+        [np.sin(th),  np.cos(th), 0.0],
         [0.0, 0.0, 1.0],
         ])
 