7ecdfb6dfbb15966d79a954eda88c2d28e654d49
zotac <zotac@hogehoge.com>
2022-08-16 01:16:02 +0900

    refactor calc_steer

--- car_model.py
+++ car_model.py
@@ -46,7 +46,7 @@ def drawCircle(ax, xy, _r=1.4, _fc='b'):
 
 
 class Car:
-    def __init__(self,ini_x=WINDOW_W/2.0,ini_y=WINDOW_H/2.0,ini_q=0.0):
+    def __init__(self):
         #robot size
         self.BODY_W = 40
         self.BODY_L = 70
@@ -71,53 +71,52 @@ class Car:
             [self.BODY_W - 8, 65, 1]
             ]).T
 
-        self.sx = ini_x
-        self.sy = ini_y
-        self.sq = ini_q 
-
         self.body = np.dot(trans(-self.BODY_W/2.0, -self.DIAMETER/2.0),self.body)
         self.tireR = np.dot(trans(self.BODY_W/2.0, -self.DIAMETER/2.0),self.tire)
         self.tireL = np.dot(mirrorX(),self.tireR)
         self.sensR = np.dot(trans(-self.BODY_W/2.0, -15),self.sens)
         self.sensL = np.dot(mirrorX(),self.sensR)
 
-    def calc_steer(self,vel_L,vel_R):
+    def calc_steer(self,sq,sx,sy,vel_L,vel_R):
         t_sq = (vel_L - vel_R) / self.BODY_W
         t_vel = (vel_L + vel_R) / 2.0
-
         #R = self.BODY_W/2.0 * (vel_R - vel_L) / (vel_R - vel_L+0.001)
         #print(f"t_sq={t_sq:.2g}, t_vel={t_vel:.2g}, R={R:.2g}")
         
-        self.sq += t_sq
-        self.sx -= t_vel * np.sin(self.sq)
-        self.sy += t_vel * np.cos(self.sq)
+        sq += t_sq
+        sx -= t_vel * np.sin(sq)
+        sy += t_vel * np.cos(sq)
 
-        if self.sx < 0:
-            self.sx += WINDOW_W
-        if self.sx >= WINDOW_W:
-            self.sx -= WINDOW_W
-        if self.sy < 0:
-            self.sy += WINDOW_H
-        if self.sy >= WINDOW_H:
-            self.sy -= WINDOW_H
-        #print(f"sq={self.sq:.2g}, sx={self.sx:.2g}, sy={self.sy:.2g}")
+        if sx < 0:
+            sx += WINDOW_W
+        if sx >= WINDOW_W:
+            sx -= WINDOW_W
+        if sy < 0:
+            sy += WINDOW_H
+        if sy >= WINDOW_H:
+            sy -= WINDOW_H
+        #print(f"sq={sq:.2g}, sx={sx:.2g}, sy={sy:.2g}")
+        return sq,sx,sy
 
 
 
 def main():
-    car = Car(400,300,np.pi)
+    car = Car()
+
+    sq = np.pi
+    sx = 400
+    sy = 300
 
     for i in range(67):  #(3.14/2)/0.025 = 68
     #for i in range(1):  #(3.14/2)/0.025 = 68
-        car.calc_steer(1,2)
+        sq,sx,sy = car.calc_steer(sq,sx,sy, 1,2)
 
-        print(f"q={car.sq:.4g}, x={car.sx:.4g}, y={car.sy:.4g}")
-
+        print(f"q={sq:.4g}, x={sx:.4g}, y={sy:.4g}")
 
         #if False:
         if True:
             ax = plt.subplot(1, 1, 1)
-            dv = trans(car.sx,car.sy) @ rot(car.sq)
+            dv = trans(sx,sy) @ rot(sq)
             print(dv)
             c_body  = dv @ car.body
             c_tireR = dv @ car.tireR
