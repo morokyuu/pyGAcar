4c882cb561db8ef7b447434e9948b38c4d7a3890
hoge <hoge@hogehoge.com>
2022-08-18 06:29:32 +0900

    sim_loop test passed

--- sim_loop.py
+++ sim_loop.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image, ImageDraw
 import car_model as cm
+import random
 
 STATE_T_NUM=3 # num of previous data
 STATE_PATTERN = 4
@@ -40,19 +41,24 @@ class SimLoop:
     def _gene2index(self,gene_part):
         return sum([s * 2**n for n,s in enumerate(gene_part[::-1])])
 
-    def _calc_wheel_speed(self,state,gene):
+    def _gene2part(self,gene,state):
         stidx = sum([s * STATE_PATTERN**n for n,s in enumerate(state[::-1])])
         idx = stidx*ACTION_NUM
         gene_part = gene[idx:idx+ACTION_NUM]
         print(f"idx={idx},gene_part={gene_part}")
+        return gene_part
+
+
+    def _calc_wheel_speed(self,state,gene):
+        gene_part = self._gene2part(gene,state)
 
         sep = ACTION_NUM//2
-        left  = self._gene2index(gene_part[:seq])
-        right = self._gene2index(gene_part[seq:ACTION_NUM])
+        left  = self._gene2index(gene_part[:sep])
+        right = self._gene2index(gene_part[sep:ACTION_NUM])
         print(f"left,right={left},{right}")
 
-        vel_L = speed_tbl[left]
-        vel_R = speed_tbl[right]
+        vel_L = self.speed_tbl[left]
+        vel_R = self.speed_tbl[right]
         return vel_L, vel_R
 
     def exec(self):
@@ -60,12 +66,12 @@ class SimLoop:
         sens = self.car.get_sens(self.pose,self.course)
         self.state = self.state + [self._sens2state(sens)]
 
-        if len(self.state) > STATE_PTTERN-1:
+        if len(self.state) > STATE_T_NUM:
             self.state = self.state[1:]
         print(self.state)
 
         # car control based on sensor state
-        self._calc_wheel_speed(self.state,self.gene)
+        vl,vr = self._calc_wheel_speed(self.state,self.gene)
 
         # calc GA score
         self.score += self._calc_score(self.state,vl,vr)
@@ -77,7 +83,9 @@ class SimLoop:
 
 def main():
     coursePix = np.array(Image.open('data/debug_course.jpg').convert('L')) 
-    sim = SimLoop(cm.Pose(400,300,np.pi),coursePix)
+    print(f"GEN_NUM={GEN_NUM}")
+    gene = list(random.choice([1,0]) for _ in range(GEN_NUM))
+    sim = SimLoop(cm.Pose(400,300,np.pi),coursePix,gene)
     for _ in range(70):
         sim.exec()
 
--- test_sim_loop.py
+++ test_sim_loop.py
@@ -8,13 +8,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image
+import car_model as cm
 import sim_loop as sl
+import random
 
 class SimLoopTest(unittest.TestCase):
     def setUp(self):
+        print("setUp===")
         coursePix = np.array(Image.open('data/debug_course.jpg').convert('L')) 
-        self.sim = SimLoop(cm.Pose(400,300,np.pi),coursePix)
-        pass
+
+        #self.gene = list(random.choice([1,0]) for _ in range(sl.GEN_NUM))
+        self.gene = [0] * sl.GEN_NUM
+
+        self.sim = sl.SimLoop(cm.Pose(400,300,np.pi),coursePix,self.gene)
 
     def tearDown(self):
         pass
@@ -24,14 +30,39 @@ class SimLoopTest(unittest.TestCase):
         idx = self.sim._gene2index(gene)
         self.assertEqual(12,idx)
 
-    def test_calc_wheel_speed(self):
-        state = [0,0,0,1]
+    def _print_separate(self,a,length):
+        spl = list(a[i:i+length] for i in range(0,len(a),length))
+        for s in spl:
+            print(s)
 
-    def test_sens_(self):
-        self.pose = cm.Pose(400,300,np.pi)
-        sens = self.car.get_sens(self.pose,self.coursePix)
-        print(f"sens(L,R) = {sens}")
-        self.assertEqual((1,0),sens)
+    def test_gene2part(self):
+        state = [3,3,3]
+        targ_gene = [1,1,1,0,0] + [1,0,0,0,1]
+
+        stidx = sum([s * sl.STATE_PATTERN**n for n,s in enumerate(state[::-1])])
+        idx = stidx*sl.ACTION_NUM
+        print(f"state,stidx,idx={state},{stidx},{idx}")
+
+        self.gene = self.gene[:idx] + targ_gene + self.gene[idx+sl.ACTION_NUM:]  
+        self._print_separate(self.gene,sl.ACTION_NUM)
+
+        print(idx)
+        val = self.sim._gene2part(self.gene,state)
+
+        self.assertEqual(idx,630)
+        self.assertEqual(val,targ_gene)
+
+#    def test_calc_wheel_speed(self):
+#        print("calc_wheel_speed 1===")
+#        state = [0,0,0,1]
+#        vl,vr = self.sim._calc_wheel_speed(state,self.gene)
+#        self.assertEqual(0,0)
+
+#    def test_sens_(self):
+#        self.pose = cm.Pose(400,300,np.pi)
+#        sens = self.car.get_sens(self.pose,self.coursePix)
+#        print(f"sens(L,R) = {sens}")
+#        self.assertEqual((1,0),sens)
 
 if __name__ == "__main__":
     unittest.main()
