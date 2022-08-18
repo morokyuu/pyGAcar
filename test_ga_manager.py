7f57e7f93dd958538ea0aa198d7d21d2f6eadf95
hoge <hoge@hogehoge.com>
2022-08-18 12:16:29 +0900

    score sort test

--- test_ga_manager.py
+++ test_ga_manager.py
@@ -10,6 +10,7 @@ import matplotlib.patches as patches
 from PIL import Image
 import ga_manager as gagm
 import random
+import sim_loop as sl
 
 class GAManagerTest(unittest.TestCase):
     def setUp(self):
@@ -21,8 +22,29 @@ class GAManagerTest(unittest.TestCase):
 
     def test_make_first_generation(self):
         self.gm.make_first_generation()
-        print(self.gm.genes)
-        self.assertEqual(0,0)
+        #print(self.gm.genes)
+
+        self.assertEqual(len(self.gm.genes),gagm.CAR_NUM)
+        self.assertEqual(len(self.gm.genes[0]),sl.GEN_NUM)
+
+    def test_choice_by_roulette(self):
+        def score():
+            return random.random()*100
+
+        genes = []
+        for i in range(gagm.CAR_NUM):
+            g = list(int(b) for b in bin(2**(sl.ACTION_NUM//2)+i)[3:]) 
+            genes += [(score(),g)]
+        #print(genes)
+
+        for gi in genes:
+            print(f"{gi[0]:5.2f},{gi[1]}")
+            #print(f"{gi[0]:.4g},{gi[1]:.4g}")
+        
+        
+
+        self.gm.choice_by_roulette(genes,2)
+        pass
 
 
 
